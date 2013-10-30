111e497439d7c048ad3c7e8495b2b622a62c6101
ZirconCode <gruenings@gmail.com>
2013-10-30 21:31:32 +0100

    play with lazy_poet

--- lazy_poet.rb
+++ lazy_poet.rb
@@ -1,3 +1,5 @@
+
+
 require 'nokogiri'
 require 'open-uri'
 require 'tactful_tokenizer'
@@ -16,19 +18,22 @@ rhymes.collect!{|x| x.first.strip}
 
 puts 'Retrieving Sentences'
 
-doc = Nokogiri::HTML(open('https://www.google.com/search?q='+URI::encode(rhymes.sample)))
+rhyme_word = rhymes.sample
+doc = Nokogiri::HTML(open('https://www.google.com/search?q='+URI::encode(rhyme_word)))
 results = doc.css('h3.r a').collect{|a| a['href'].scan(/url\?q=(.*?)&/).first}
 
 
 doc  = Nokogiri::HTML(open(results.first.first))
 
 m = TactfulTokenizer::Model.new
-pot_sentences = m.tokenize_text(doc.text).collect{|s| s unless s.include?('{')}
-# TODO here Clean the text results & continue =)
+pot_sentences = m.tokenize_text(doc.text).collect{|s| s if s.index(/(\w+\s\w+\s\w+\s#{rhyme_word})/i)}
+pot_sentences.reject!{|s| s==nil }
+# TODO get the regex&tokenizer to give proper results...
 
 
 puts pot_sentences
 
 puts 'Rhyming ^_^/'
+puts rhyme_word
 
 
