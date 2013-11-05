ef890b41c7ab3fd61fc54364d03c815095ef5a7f
ZirconCode <gruenings@gmail.com>
2013-11-05 21:40:29 +0100

    add elance_comp...

---
+++ elance_competition_analysis.rb
@@ -0,0 +1,24 @@
+
+# Analyze Your Competition for a Job on elance.com
+# just an idea right now =)
+
+# by ZirconCode
+
+def get_person(slug)
+	#https://www.elance.com/s/ + slug
+
+	# gets skills, keyword extraction from overview, hourly, etc... 
+end
+
+
+def get_job(slug)
+	#https://www.elance.com/j/ + slug
+
+	# gets proposals->people
+end
+
+
+# get job
+# get people
+# display statistics of people
+# ?compare to you?
