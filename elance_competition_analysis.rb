069993959f023ee30681405b87e52e56eeef2661
ZirconCode <gruenings@gmail.com>
2013-11-06 11:19:44 +0100

    add elance... get_person()

--- elance_competition_analysis.rb
+++ elance_competition_analysis.rb
@@ -1,12 +1,57 @@
 
 # Analyze Your Competition for a Job on elance.com
-# just an idea right now =)
 
 # by ZirconCode
 
-def get_person(slug)
-	#https://www.elance.com/s/ + slug
+require 'nokogiri'
+require 'open-uri'
+require 'pp'
 
+$Cache = Hash.new
+def cache(url)
+	if !$Cache[:site]
+		doc = Nokogiri::HTML(open(url))
+		$Cache[:site] = doc
+	end
+
+	$Cache[:site]
+end
+
+def save_cache
+	# json stuff
+end
+
+def load_cache
+	# json stuff
+end
+
+
+def clean(string)
+	string.delete("\n").delete("\t")
+end
+
+def get_person(slug)
+
+	person = Hash.new
+	url = 'https://www.elance.com/s/'+slug
+	doc = cache(url)
+
+	person[:slug] = slug
+	person[:name] = doc.xpath("//*[@id='p-title']/text()").text.strip
+	person[:tagline] = doc.xpath("//*[@id='p-hd']/div[1]/span").text
+
+	overview = doc.xpath("//*[@id='p-overview']/div[1]/p").text
+	person[:overview] = clean(overview)
+
+	skills = doc.xpath("//*[@id='profileSkills']/tbody/tr/td/div/text()")
+	skills = skills.map{|s| clean(s.text)}.reject{|i| !i || i.empty?}
+	person[:skills] = skills
+
+	keywords = doc.xpath("//*[@id='p-keywords']/div/div/text()")
+	keywords = keywords.map{|s| clean(s.text)}.reject{|i| !i || i.empty?}
+	person[:keywords] = keywords
+
+	person
 	# gets skills, keyword extraction from overview, hourly, etc... 
 end
 
@@ -22,3 +67,6 @@ end
 # get people
 # display statistics of people
 # ?compare to you?
+
+pp get_person('simon-g')
+
